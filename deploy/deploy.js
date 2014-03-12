68631abac943b834ad02a547b5a4e7a0fd1810cf
Jason Norwood-Young <jason@10layer.com>
2014-03-12 11:32:32 +0200

    Some textual changes

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -7,10 +7,10 @@ Code4SA.template = (function(window,document,undefined) {
 <div class="btn btn-danger btn-sm" id="close"><span class="glyphicon glyphicon-remove"></span></div> \
 <div id="vizcontainer" class="col-md-12"> \
 <div id="titlecontainer" class="svg-container"></div> \
-<div id="howto" class="text-center"> \
+<div id="howto"> \
 	<p>The graph plots parties\' candidates according to median age and percentage of women. The number of candidates is indicated by circle size.</p>\
 	<p>Move your mouse over the relevant circle for more information about the party.</p> \
-	<p><strong>Check back on 22 April 2014 for an updated list with the 2014 Candidates</strong></p>\
+	<p><strong>Check back after April 22 2014 for an updated list with the 2014 candidates.</strong></p>\
 </div> \
 <div id="quadcontainer" ></div> \
 <p><small>Source: IEC - 2009 National and Regional Candidates List</small></p> \
