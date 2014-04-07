470e1de923152865cb6667c51bf1e600ac7c28d6
Jason Norwood-Young <jason@10layer.com>
2014-04-07 20:05:28 +0200

    Add google analytics

--- viz/js/scatter.js
+++ viz/js/scatter.js
@@ -455,6 +455,22 @@ Code4SA.app = (function(window,document,undefined) {
                 el.style.height = (parseInt(window.innerHeight) - 20) + "px";
             }
         }
+        _gaq.push(['_setAccount', 'UA-48399585-4']);
+        _gaq.push(['_trackPageview']);
+        (function () {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+
+        // if (window.ga) {
+        //     window.ga('create', 'UA-48399585-4', 'citypress.co.za');
+        //     window.ga('send', 'pageview');
+        // }
     }; //Init
 
 
@@ -502,4 +518,5 @@ Code4SA.app = (function(window,document,undefined) {
     return {
         init: function() { return self.init(); }
     };
+
 })(this, this.document);
